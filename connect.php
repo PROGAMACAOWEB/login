2cd41f49723f46826a75b64be77fc6291b0e4fde
Pedro Moreira <pmoreira.estg@gmail.com>
2016-06-06 20:40:48 +0100

    update

--- connect.php
+++ connect.php
@@ -1,14 +1,17 @@
 <?php
+// info de autenticação no SGBD
 $userdb = "phpweb";
 $passdb = "phpppp";
 $hostdb = "localhost";
-$namedb = "PWTPSI";
+$namedb = "pwtpsi";
 
-
+// estabelecer ligação ao SGBD
 $myconn = new mysqli($hostdb,$userdb,$passdb,$namedb);
 
+// verifica se existe erro de ligação
 if ($myconn->connect_errno) {
+    // se existir, sai com mensagem de erro
     //die ($myconn->connect_error);
-    die("Erro na BD");
+    die("Erro na BD"); // 'die' -> o mesmo que exit
 }
 ?>
